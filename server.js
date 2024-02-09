2f650334fd01263208e4d54ed9f05585477b7b4e
shihab-fms <shahshiful@gmail.com>
2024-02-09 18:16:14 +0600

    DB update

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<password>',
-  process.env.DATABASE_PASSWORD,
-);
+const DB =
+  `mongodb+srv://shahshiful:<password>@cluster0.ubmnbga.mongodb.net/`.replace(
+    '<password>',
+    process.env.DATABASE_PASSWORD,
+  );
 
 mongoose.set('strictQuery', false);
 
